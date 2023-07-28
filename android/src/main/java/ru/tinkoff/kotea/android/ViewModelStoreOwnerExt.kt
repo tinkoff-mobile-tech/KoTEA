68bea5788ecd5524499e5e4d4a7b58c80403c5ca
a.a.korshun <lehakorshun@gmail.com>
2023-07-28 11:09:08 +0600

    revert old methods for binary compatible

--- android/src/main/java/ru/tinkoff/kotea/android/ViewModelStoreOwnerExt.kt
+++ android/src/main/java/ru/tinkoff/kotea/android/ViewModelStoreOwnerExt.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStore
 import androidx.lifecycle.ViewModelStoreOwner
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.plus
 import ru.tinkoff.kotea.core.Store
@@ -31,6 +32,29 @@ fun <T : Store<*, *, *>> storeViaViewModel(
             return value ?: run {
                 val key = sharedViewModelKey ?: keyFromProperty(thisRef, property)
                 val vm = thisRef.viewModelStore.get(key) { StoreViewModel(factory(), coroutineContext) }
+                vm.store.also { value = it }
+            }
+        }
+
+        private fun keyFromProperty(thisRef: ViewModelStoreOwner, property: KProperty<*>): String {
+            return thisRef::class.java.canonicalName!! + "#" + property.name
+        }
+    }
+}
+
+@Deprecated("Use storeViewViewModel function with coroutine context param")
+fun <T : Store<*, *, *>> storeViaViewModel(
+    dispatcher: CoroutineDispatcher = Dispatchers.Default,
+    sharedViewModelKey: String? = null,
+    factory: () -> T
+): ReadOnlyProperty<ViewModelStoreOwner, T> {
+    return object : ReadOnlyProperty<ViewModelStoreOwner, T> {
+        private var value: T? = null
+
+        override fun getValue(thisRef: ViewModelStoreOwner, property: KProperty<*>): T {
+            return value ?: run {
+                val key = sharedViewModelKey ?: keyFromProperty(thisRef, property)
+                val vm = thisRef.viewModelStore.get(key) { StoreViewModel(factory(), dispatcher) }
                 vm.store.also { value = it }
             }
         }
